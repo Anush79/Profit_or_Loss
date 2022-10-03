f88de80262e5958e38709230b8b24183d26d1d0f
Anush79 <66175237+Anush79@users.noreply.github.com>
2022-10-03 23:45:24 +0530

    final check done, made responsive

--- script.js
+++ script.js
@@ -78,23 +78,3 @@ resetbtn.addEventListener("click",function(){
   window.location.reload();
 })
 
-//Profit percentage (P%) = (Profit /Cost Price) x 100
-// Loss percentage (L%) = (Loss / Cost price) x 100
-
-// Profit or Gain = Selling price – Cost Price
-// Loss = Cost Price – Selling Price
-
-// What to do----
-//
-// Take user's stock price for one stock when he bought it
-// Take the quantity as well
-
-// Now, take today's stock price for that stock and
-
-// Tell the user how much profit or loss he/she is making
-
-// tell it in percentage
-// tell it in absolute too
-// bonus
-// use right emoji or graphics to show profit/loss
-// can you change the entire theme of the app to sad if someone has lost more than 50%
